a6ce1eb16950f012bc17cb9edde34002d1b1b899
OneLetterShor <OneEmailShor@gmail.com>
2026-06-05 23:25:42 -0500

    feat: Implement ascension tagging

--- src/HideAndSeek/Hooking/RainWorldHooks.cs
+++ src/HideAndSeek/Hooking/RainWorldHooks.cs
@@ -1,3 +1,6 @@
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
 using OneLetterShor.HideAndSeek.Arena;
 using OneLetterShor.HideAndSeek.Utils;
 using RainMeadow;
@@ -10,6 +13,146 @@ public static class RainWorldHooks
     {
         On.Player.Collide += On_Player_Collide;
         On.Rock.HitSomething += On_Rock_HitSomething;
+        
+        _ = new ILHook(
+            typeof(Player).GetMethod(
+                nameof(Player.ClassMechanicsSaint),
+                BindingFlags.Public | BindingFlags.Instance
+            ),
+            IL_Player_ClassMechanicsSaint
+        );
+    }
+    
+    private static void IL_Player_ClassMechanicsSaint(ILContext il)
+    {
+        /*
+         (last updated: 6/2/26)
+         
+         
+         Current code:
+         ...
+         foreach (BodyChunk bodyChunk in physicalObject.bodyChunks)
+         {
+             if (Custom.DistLess(bodyChunk.pos, vector2, num6 + bodyChunk.rad) && this.room.VisualContact(bodyChunk.pos, vector2))
+             {
+                 bodyChunk.vel += Custom.RNV() * 36f;
+                 if (physicalObject is Creature)
+                 {
+                     if (!(physicalObject as Creature).dead)
+                         flag2 = true;
+                     (physicalObject as Creature).Die();
+                 }
+         ...
+         
+         Note: Rain Meadow hooks the same part of this method. (Arena/ArenaHooks.cs:1633)
+        
+         Desired code (excluding Rain Meadow's emitted instructions):
+         ...
+         foreach (BodyChunk bodyChunk in physicalObject.bodyChunks)
+         {
+             if (Custom.DistLess(bodyChunk.pos, vector2, num6 + bodyChunk.rad) && this.room.VisualContact(bodyChunk.pos, vector2))
+             {
+                 bodyChunk.vel += Custom.RNV() * 36f;
+                 if (physicalObject is Creature)
+                 {
+                     if (!(physicalObject as Creature).dead)
+                         flag2 = true;
+                     
+                     if (HideAndSeekMode.IsHideAndSeekMode(out HideAndSeekMode? hideAndSeek) ||
+                         otherPO is Player otherPlayer)
+                     {
+                         OnlineCreature saintOCreature = saintPlayer.abstractCreature.GetOnlineCreature()!;
+                         OnlineCreature otherOCreature = otherPlayer.abstractCreature.GetOnlineCreature()!;
+                         
+                         if (saintOCreature.isMine &&
+                             saintOCreature.owner.IsSeeker &&
+                             !otherOCreature.owner.IsSeeker)
+                         {
+                             hideAndSeek.MakeSeeker(otherOCreature.owner);
+                         }
+                         
+                         return true;
+                     }
+                     else
+                         (physicalObject as Creature).Die();
+                 }
+         ...
+         
+         example of the instructions (simplified)
+         
+         physObj                      // Emitted from Hide N Seek
+         self                         // Emitted from Hide N Seek
+         <Func(self, physObj, bool)>  // Emitted from Hide N Seek
+         brtrue skip <-               // Emitted from Hide N Seek
+         physObj
+         isinst Creature
+         self                         // Emitted from Rain Meadow
+         physObj                      // Emitted from Rain Meadow
+         <Action(self, physObj)>      // Emitted from Rain Meadow
+         Die()
+         skip:                        // Emitted from Hide N Seek
+        */
+        
+        // TODO: Disable flinging and sound when the hit player is not a hider and when the ascender is not a seeker.
+        try
+        {
+            ILCursor cursor = new(il);
+            ILLabel skip = cursor.DefineLabel();
+            
+            const int locIndex18 = 18; // No source code name provided, physical object that is used to kill if ascended.
+            
+            cursor.GotoNext(
+                MoveType.After,
+                i => i.MatchCallvirt<Creature>(nameof(Creature.Die)) // The only usage of Die() is when saint is successfully ascending something.
+            );
+            
+            cursor.MarkLabel(skip);
+            
+            // TODO: Why the slug does there need to be two GotoPrev()s when the instructions are back to back. (No other IL hook is interfering, the index change from one to the next is 1)
+            
+            // Move before the instructions needed to call Die().
+            cursor.GotoPrev(
+                MoveType.Before,
+                i => i.MatchIsinst<Creature>()
+            ).GotoPrev(
+                MoveType.Before,
+                i => i.MatchLdloc(locIndex18)
+            );
+            
+            // Skip Rain World and Rain Meadow code if the game mode is Hide N Seek.
+            // Determine if the ascension should tag someone.
+            
+            cursor.Emit(OpCodes.Ldarg, 0);
+            cursor.Emit(OpCodes.Ldloc, locIndex18);
+            cursor.EmitDelegate(
+                (Player saintPlayer, PhysicalObject otherPO) =>
+                {
+                    if (!HideAndSeekMode.IsHideAndSeekMode(out HideAndSeekMode? hideAndSeek) ||
+                        otherPO is not Player otherPlayer
+                    )
+                        return false;
+                    
+                    OnlineCreature saintOCreature = saintPlayer.abstractCreature.GetOnlineCreature()!;
+                    OnlineCreature otherOCreature = otherPlayer.abstractCreature.GetOnlineCreature()!;
+                    
+                    if (saintOCreature.isMine &&
+                        saintOCreature.owner.IsSeeker &&
+                        !otherOCreature.owner.IsSeeker)
+                    {
+                        Logger.Debug($"{saintOCreature.owner} hit {otherOCreature.owner} with ascension!");
+                        hideAndSeek.MakeSeeker(otherOCreature.owner);
+                    }
+                    
+                    return true;
+                }
+            );
+            
+            cursor.Emit(OpCodes.Brtrue, skip);
+        }
+        catch (Exception exception)
+        {
+            Logger.Fatal(exception);
+        }
     }
     
     private static bool On_Rock_HitSomething(
