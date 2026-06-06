bd9db0e24db7364f64acd276822b2faff05fe95b
OneLetterShor <OneEmailShor@gmail.com>
2026-06-05 22:58:21 -0500

    chore: Create IsSeeker and CanEnableNametags extension properties

---
+++ src/HideAndSeek/Utils/OnlinePlayerExtensions.cs
@@ -0,0 +1,46 @@
+using OneLetterShor.HideAndSeek.Arena;
+using RainMeadow;
+
+namespace OneLetterShor.HideAndSeek.Utils;
+
+public static class OnlinePlayerExtensions
+{
+    extension(OnlinePlayer oPlayer)
+    {
+        /// <summary>
+        /// Checks if the <see cref="OnlinePlayer"/> is a seeker or not.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown when:<br/>
+        /// - The online game mode is not <see cref="ArenaOnlineGameMode"/>.<br/>
+        /// - The external game mode is not <see cref="HideAndSeekMode"/>.
+        /// </exception>
+        /// <exception cref="KeyNotFoundException">Thrown if the lobby data is not registered yet.</exception>
+        public bool IsSeeker
+        {
+            get
+            {
+                if (OnlineManager.lobby!.gameMode is not ArenaOnlineGameMode arenaOnline)
+                    throw new InvalidOperationException($"The online game mode ({OnlineManager.lobby.gameMode}) is not arena.");
+                
+                if (!HideAndSeekMode.IsHideAndSeekMode(out HideAndSeekMode? hideAndSeek))
+                    throw new InvalidOperationException($"The external game mode ({arenaOnline.currentGameMode}) is not Hide and Seek.");
+                
+                return hideAndSeek.LobbyData.Seekers.Contains(oPlayer);
+            }
+        }
+        
+        
+        /// <summary>
+        /// Checks if an <see cref="OnlinePlayer"/>
+        /// is allowed to turn on name tags.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown when:<br/>
+        /// - The online game mode is not <see cref="ArenaOnlineGameMode"/>.<br/>
+        /// - The external game mode is not <see cref="HideAndSeekMode"/>.
+        /// </exception>
+        /// <exception cref="KeyNotFoundException">Thrown if the lobby data is not registered yet.</exception>
+        public bool CanEnableNametags => !oPlayer.IsSeeker;
+    }
+}
